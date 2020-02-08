e6e6ef2cc595bab019a4fb1da5beef1f739c1bc4
Astrian Zheng <git@astrianzheng.cn>
2020-02-08 16:27:20 +0800

    delete debug codes

--- debug.js
+++
@@ -1,9 +0,0 @@
-const api = require('./index')
-
-main()
-
-async function main(){
-  let sth = await api.recent({ ult: '5fd232283ad3405e5e9cb9bff1cea187', userId: '2308974580094250' })
-  console.log(sth.result)
-  console.log(sth.token)
-}
